a1f3d2096206521033acc77719cd2964c0d47372
Eric Vallee <eric_vallee@webificservices.com>
2023-03-24 09:08:15 -0400

    feat: INFRA-306 Initial implementation

---
+++ main.tf
@@ -0,0 +1,193 @@
+locals {
+  cloud_init_volume_name = var.cloud_init_volume_name == "" ? "${var.name}-cloud-init.iso" : var.cloud_init_volume_name
+  network_interfaces = length(var.macvtap_interfaces) == 0 ? [{
+    network_name = var.libvirt_network.network_name != "" ? var.libvirt_network.network_name : null
+    network_id = var.libvirt_network.network_id != "" ? var.libvirt_network.network_id : null
+    macvtap = null
+    addresses = [var.libvirt_network.ip]
+    mac = var.libvirt_network.mac != "" ? var.libvirt_network.mac : null
+    hostname = var.name
+  }] : [for macvtap_interface in var.macvtap_interfaces: {
+    network_name = null
+    network_id = null
+    macvtap = macvtap_interface.interface
+    addresses = null
+    mac = macvtap_interface.mac
+    hostname = null
+  }]
+}
+
+module "network_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//network?ref=main"
+  network_interfaces = var.macvtap_interfaces
+}
+
+module "ssh_tunnel_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//ssh-tunnel?ref=feature/transport-load-balancer"
+  ssh_host_key_rsa = var.ssh_host_key_rsa
+  ssh_host_key_ecdsa = var.ssh_host_key_ecdsa
+  tunnel = {
+    ssh = var.ssh_tunnel.ssh
+    accesses = [{
+      host = "127.0.0.1"
+      port = "*"
+    }]
+  }
+}
+
+module "transport_load_balancer_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//transport-load-balancer?ref=feature/transport-load-balancer"
+  install_dependencies = var.install_dependencies
+  control_plane = var.control_plane
+  load_balancer = {
+    cluster = var.name
+    node_id = var.name
+  }
+}
+
+module "prometheus_node_exporter_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//prometheus-node-exporter?ref=main"
+  install_dependencies = var.install_dependencies
+}
+
+module "chrony_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//chrony?ref=main"
+  install_dependencies = var.install_dependencies
+  chrony = {
+    servers  = var.chrony.servers
+    pools    = var.chrony.pools
+    makestep = var.chrony.makestep
+  }
+}
+
+module "fluentd_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//fluentd?ref=main"
+  install_dependencies = var.install_dependencies
+  fluentd = {
+    docker_services = []
+    systemd_services = [
+      {
+        tag     = var.fluentd.load_balancer_tag
+        service = "transport-load-balancer"
+      },
+      {
+        tag     = var.fluentd.control_plane_tag
+        service = "transport-control-plane"
+      },
+      {
+        tag     = var.fluentd.node_exporter_tag
+        service = "node-exporter"
+      }
+    ]
+    forward = var.fluentd.forward,
+    buffer = var.fluentd.buffer
+  }
+}
+
+locals {
+  cloudinit_templates = concat([
+      {
+        filename     = "base.cfg"
+        content_type = "text/cloud-config"
+        content = templatefile(
+          "${path.module}/files/user_data.yaml.tpl", 
+          {
+            hostname = var.name
+            ssh_admin_public_key = var.ssh_admin_public_key
+            ssh_admin_user = var.ssh_admin_user
+            admin_user_password = var.admin_user_password
+          }
+        )
+      },
+      {
+        filename     = "transport_load_balancer.cfg"
+        content_type = "text/cloud-config"
+        content      = module.transport_load_balancer_configs.configuration
+      },
+      {
+        filename     = "node_exporter.cfg"
+        content_type = "text/cloud-config"
+        content      = module.prometheus_node_exporter_configs.configuration
+      }
+    ],
+    var.ssh_tunnel.enabled ? [{
+      filename     = "ssh_tunnel.cfg"
+      content_type = "text/cloud-config"
+      content      = module.ssh_tunnel_configs.configuration
+    }] : [],
+    var.chrony.enabled ? [{
+      filename     = "chrony.cfg"
+      content_type = "text/cloud-config"
+      content      = module.chrony_configs.configuration
+    }] : [],
+    var.fluentd.enabled ? [{
+      filename     = "fluentd.cfg"
+      content_type = "text/cloud-config"
+      content      = module.fluentd_configs.configuration
+    }] : [],
+  )
+}
+
+data "template_cloudinit_config" "user_data" {
+  gzip = false
+  base64_encode = false
+  dynamic "part" {
+    for_each = local.cloudinit_templates
+    content {
+      filename     = part.value["filename"]
+      content_type = part.value["content_type"]
+      content      = part.value["content"]
+    }
+  }
+}
+
+resource "libvirt_cloudinit_disk" "k8_node" {
+  name           = local.cloud_init_volume_name
+  user_data      = data.template_cloudinit_config.user_data.rendered
+  network_config = length(var.macvtap_interfaces) > 0 ? module.network_configs.configuration : null
+  pool           = var.cloud_init_volume_pool
+}
+
+resource "libvirt_domain" "k8_node" {
+  name = var.name
+
+  cpu {
+    mode = "host-passthrough"
+  }
+
+  vcpu = var.vcpus
+  memory = var.memory
+
+  disk {
+    volume_id = var.volume_id
+  }
+
+  dynamic "network_interface" {
+    for_each = local.network_interfaces
+    content {
+      network_id = network_interface.value["network_id"]
+      network_name = network_interface.value["network_name"]
+      macvtap = network_interface.value["macvtap"]
+      addresses = network_interface.value["addresses"]
+      mac = network_interface.value["mac"]
+      hostname = network_interface.value["hostname"]
+    }
+  }
+
+  autostart = true
+
+  cloudinit = libvirt_cloudinit_disk.k8_node.id
+
+  //https://github.com/dmacvicar/terraform-provider-libvirt/blob/main/examples/v0.13/ubuntu/ubuntu-example.tf#L61
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+}
